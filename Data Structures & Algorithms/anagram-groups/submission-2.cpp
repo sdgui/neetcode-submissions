4de1b639325cf6bbad14c7387e42054b233fd67c
Ahmet Mete Atay <62745706+sdgui@users.noreply.github.com>
2026-07-04 21:05:15 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<int,vector<string>> numofChars;
+        vector<vector<string>> final;
+        for (int i=0; i<strs.size(); i++){
+            numofChars[strs[i].length()].push_back(strs[i]);
+        }
+        for (auto pair: numofChars){
+            vector<string> lst=pair.second;
+            unordered_map<int, int[26]> counts;
+            unordered_map<int, vector<string>> groups; 
+            for (int i=0; i<lst.size(); i++){
+                for (char a: lst[i]){
+                    counts[i][a-'a'] += 1; 
+                }
+                bool found=false;
+                for (int j=0; j<i; j++){
+                    if (sameCounts(counts[i], counts[j])){
+                        groups[j].push_back(lst[i]);
+                        found=true;
+                        break;
+                    }
+                }
+                if (!found){
+                    vector<string>group={lst[i]};
+                    groups[i]=group;
+                }
+            }
+            for (auto mapGroup: groups){
+                final.push_back(mapGroup.second);
+            }
+        }
+        return final;
+    }
+
+    bool sameCounts(int a[], int b[]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
